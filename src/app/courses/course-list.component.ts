935949904adb6e90a417e316d39712e738ab8bf4
Tayna Rodrigues <taynajesus@gmail.com>
2020-04-25 00:52:45 -0400

    Primeiros passos para desenvolver com Angular

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit{
+
+    courses: Course[] = [];
+
+    ngOnInit(): void{
+        this.courses = [
+            {
+             id: 1,
+             name: 'Angular: Forms',
+             imageUrl: '',
+             price: 99.99,
+             code: 'xps-8796',
+             duration: 120,
+             rating: 5.4,
+             releaseDate: 'Abril, 24, 2020'
+          },
+          {
+            id: 2,
+            name: 'Angular: HTTP',
+            imageUrl: '',
+            price: 45.99,
+            code: 'lkl-8796',
+            duration: 80,
+            rating: 4,
+            releaseDate: 'Maio, 24, 2020'
+         }
+        ]
+    }
+
+}
