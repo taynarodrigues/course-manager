a5fb259348b7efb11f0e8f06356fcf6f4cf775dc
Tayna Rodrigues <taynajesus@gmail.com>
2020-04-25 02:08:39 -0400

    Criando componentes com atributos interdependentes, Angular 8

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -15,17 +15,17 @@ export class CourseListComponent implements OnInit{
             {
              id: 1,
              name: 'Angular: Forms',
-             imageUrl: '',
+             imageUrl: '/assets/imagens/forms.png',
              price: 99.99,
              code: 'xps-8796',
              duration: 120,
-             rating: 5.4,
+             rating: 4.5,
              releaseDate: 'Abril, 24, 2020'
           },
           {
             id: 2,
             name: 'Angular: HTTP',
-            imageUrl: '',
+            imageUrl: '/assets/imagens/http.png',
             price: 45.99,
             code: 'lkl-8796',
             duration: 80,
